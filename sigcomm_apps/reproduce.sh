f09916be84cca67e8aaa4ccf0fa87db82c0cca00
jsonch <jsonch@seas.upenn.edu>
2021-05-22 20:36:03 -0400

    executable

--- sigcomm_apps/reproduce.sh
+++ sigcomm_apps/reproduce.sh
@@ -1,9 +1,9 @@
 # compile the p4 to tofino if the SDE exists.
 if [ -d "/home/vagrant/bf-sde-9.5.0" ]
 then
-    SDE=/home/vagrant/bf-sde-9.5.0
-    SDE_INSTALL=/home/vagrant/bf-sde-9.5.0/install
-    PATH=$PATH:$SDE_INSTALL/bin
+    export SDE=/home/vagrant/bf-sde-9.5.0
+    export SDE_INSTALL=/home/vagrant/bf-sde-9.5.0/install
+    export PATH=$PATH:$SDE_INSTALL/bin
     cd ~/lucid/sigcomm_apps
     make p4
     make tofino
