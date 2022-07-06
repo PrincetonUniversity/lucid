382e9ef93574439f0b16db05c0754de6c0dff16b
jsonch <jsonch@seas.upenn.edu>
2022-07-06 11:14:40 -0400

    interpStream stub, notes

---
+++ src/lib/midend/interpreter/InterpStream.ml
@@ -0,0 +1,47 @@
+open Batteries
+open CoreSyntax
+open InterpCore
+open InterpState
+open Collections
+
+type t =
+  { events : (event_sort * ty list) Env.t
+  ; externs : ty Env.t
+  ; extern_funs : IdSet.t
+  }
+
+let empty =
+  { events = Env.empty
+  ; externs =
+      Env.singleton
+        (Id Builtins.recirc_id)
+        (SyntaxToCore.translate_ty Builtins.recirc_ty)
+  ; extern_funs = IdSet.empty
+  }
+;;
+
+(* Remove and process declarations which we can/must do beforehand --
+   extern/event declarations, and precompute size values *)
+let preprocess ds =
+  let pp, ds =
+    List.fold_left
+      (fun (pp, ds) d ->
+        match d.d with
+        | DEvent (id, sort, params) ->
+          ( { pp with
+              events = Env.add (Id id) (sort, List.map snd params) pp.events
+            }
+          , d :: ds )
+        | DExtern (id, ty) ->
+          begin
+            match ty.raw_ty with
+            | TFun _ ->
+              { pp with extern_funs = IdSet.add id pp.extern_funs }, ds
+            | _ -> { pp with externs = Env.add (Id id) ty pp.externs }, ds
+          end
+        | _ -> pp, d :: ds)
+      (empty, [])
+      ds
+  in
+  pp, List.rev ds
+;;
