6c0447df8bfd78aedae400f6ee0183d967327906
Devon Loehr <dloehr@princeton.edu>
2023-03-27 11:34:05 -0400

    Fiddle with Frontend Pipeline to see what's necessary and what can be reordered

--- src/lib/frontend/transformations/ModuleAliasing.ml
+++ src/lib/frontend/transformations/ModuleAliasing.ml
@@ -19,21 +19,21 @@ let rec alias_helper acc ds =
   | [] -> List.rev acc
   | hd :: tl ->
     (match hd.d with
-    | DModuleAlias (id, e, cid1, cid2) ->
-      let cid =
-        match e.e with
-        | EVal { v = VBool b } -> if b then cid1 else cid2
-        | _ ->
-          Console.error_position hd.dspan
-          @@ "Module aliasing booleans must be either symbolic or constant"
-      in
-      let tl = aliaser#visit_decls (id, cid) tl in
-      alias_helper acc tl
-    | DModule (id, intf, ds') ->
-      let ds' = alias_helper [] ds' in
-      let new_d = { hd with d = DModule (id, intf, ds') } in
-      alias_helper (new_d :: acc) tl
-    | _ -> alias_helper (hd :: acc) tl)
+     | DModuleAlias (id, e, cid1, cid2) ->
+       let cid =
+         match e.e with
+         | EVal { v = VBool b } -> if b then cid1 else cid2
+         | _ ->
+           Console.error_position hd.dspan
+           @@ "Module aliasing booleans must be either symbolic or literal"
+       in
+       let tl = aliaser#visit_decls (id, cid) tl in
+       alias_helper acc tl
+     | DModule (id, intf, ds') ->
+       let ds' = alias_helper [] ds' in
+       let new_d = { hd with d = DModule (id, intf, ds') } in
+       alias_helper (new_d :: acc) tl
+     | _ -> alias_helper (hd :: acc) tl)
 ;;
 
 let alias_prog = alias_helper []
