8b081310067485713eeb236489daf8620ce7d591
Devon Loehr <dloehr@princeton.edu>
2023-05-04 15:14:19 -0400

    Update testing script so that tests are expected to fail iff they end in _wrong

--- test/runtests.py
+++ test/runtests.py
@@ -14,10 +14,22 @@ parserfiles = [x for x in os.listdir(parserdir) if x.endswith(".dpt")]
 poplfiles = [x for x in os.listdir(popldir) if x.endswith(".dpt")]
 
 errors = []
+bad_successes = []
 diffs = []
 
 if not (os.path.isdir("test/output")):
     os.mkdir("test/output")
+
+# Convention: a test is expected to fail if and only if the test file ends in
+# _wrong.dpt
+def check_return(ret, fullfile):
+    expect_error = fullfile.endswith("_wrong.dpt")
+    if ret.returncode != 0 and (not expect_error):
+         print("test returned error: "+"./dpt --silent %s"%fullfile)
+         errors.append(fullfile)
+    if ret.returncode == 0 and expect_error:
+        print("test unexpectedly did NOT return error: "+"./dpt --silent %s"%fullfile)
+        bad_successes.append(fullfile)
 
 def interp_test(fullfile, args):
     shortfile = fullfile[0:-4]
@@ -27,10 +39,7 @@ def interp_test(fullfile, args):
         fullfile = interpdir+fullfile
         cmd = ["./dpt", "--silent", fullfile] + args
         ret = subprocess.run(cmd, stdout=outfile, stderr=subprocess.DEVNULL)
-    if ret.returncode != 0:
-        # errors might be expected
-        print("test returned error: "+"./dpt --silent %s"%fullfile)
-        errors.append(fullfile)
+    check_return(ret, fullfile)
     if not filecmp.cmp("test/output/"+outname, "test/expected/"+outname):
         print("test returned different output than expected: "+"./dpt --silent %s"%fullfile)
         diffs.append(shortfile)
@@ -41,9 +50,8 @@ def just_typecheck(path, file, suffix = ""):
     fullfile = path+file+suffix
     cmd = ["./dpt", "--silent",  fullfile]
     ret = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if ret.returncode != 0:
-        print("test returned error: "+"./dpt --silent %s"%fullfile)
-        errors.append(fullfile)
+    check_return(ret, fullfile)
+
 
 def interactive_test(fullfile, args):
     shortfile = fullfile[0:-4]
@@ -75,4 +83,5 @@ for file in poplfiles: just_typecheck(popldir, file)
 for file in interactivefiles: interactive_test(file, [])
 
 print("Diffs:", diffs)
-print("Errors (note: errors may be expected for some programs):", errors)
+print("Unexpected error:", errors)
+print("Unexpected success:", bad_successes)
