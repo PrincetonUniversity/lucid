263dfa6f27636b0f11faa9b338ef6b7955414c93
Devon Loehr <dloehr@princeton.edu>
2023-05-05 14:04:21 -0400

    Standardize the ocaml interfaces for our builtin lucid libraries

---
+++ src/lib/frontend/modules/LibraryInterface.ml
@@ -0,0 +1,21 @@
+open InterpState
+open Syntax
+
+(* All libraries should have this interface *)
+module type BasicInterface = sig
+  val module_id : Id.t
+  val defs : State.global_fun list
+
+  val signature:
+    Id.t * (Id.t * sizes * ty) list * State.global_fun list * (Cid.t * func_ty) list
+  [@@ocamlformat "disable"]
+end
+
+(* Libraries which define a type should have this interface *)
+module type TypeInterface = sig
+  include BasicInterface
+
+  val t_id : Cid.t
+  val sizes_labels : Id.t list * params
+  val constructors : (Cid.t * func_ty) list
+end
--- src/lib/frontend/modules/System.mli
+++ src/lib/frontend/modules/System.mli
@@ -1,9 +1,1 @@
-open InterpState
-open Syntax
-
-val module_id : Id.t
-val defs : State.global_fun list
-
-val signature:
-  Id.t * (Id.t * sizes * ty) list * State.global_fun list * (Cid.t * func_ty) list
-[@@ocamlformat "disable"]
+include LibraryInterface.BasicInterface
