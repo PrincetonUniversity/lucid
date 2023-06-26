ef595466291b9536e96b2ac72bf15342655964d8
John Sonchack <john.sonchack@gmail.com>
2023-06-26 15:53:16 -0400

    sketching out simpler final translation pass

---
+++ src/lib/backend/TofinoCoreToP4.ml
@@ -0,0 +1,7 @@
+(* translate new tofinocore (6/24) into p4tofino syntax -- hopefully more direct... *)
+
+open TofinoCoreNew
+open P4TofinoSyntax
+
+
+(* let prog_to_tofinoprog prog : tofino_prog =  *)
