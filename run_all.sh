cbefc6d7c07feb83c5cfa5037940ae0cb736eca2
Andrew Johnson <aj3189@princeton.edu>
2023-09-26 19:21:15 -0400

    fix some orderings and run_all changes

--- run_all.sh
+++ run_all.sh
@@ -6,7 +6,7 @@
 # Loop through each filename in files.txt
 while IFS= read -r filename; do
     # Time the dptc command and run it, redirecting output to output.txt
-    { time ./dptc "$filename"; } 2>> output.txt
+    { time ./dptc "$filename"; } > output.txt 2>&1
 
     # Extract synthesis time, alphabet size, and states size using grep and regular expressions
     synthesis_time=$(grep -oE "Time spent on synthesis is ([0-9.]+)" output.txt | sed 's/Time spent on synthesis is //')
@@ -24,14 +24,14 @@ while IFS= read -r filename; do
     echo "dptc Runtime for $filename: $dptc_time" >> results.txt
 
     # Time the bf-p4c command and run it
-    { time ~/p4c-9.13.0.x86_64/bin/bf-p4c -g "./lucid_tofino_build/lucid.p4"; } 2> bf-p4c_time.txt
+    { time ~/p4c-9.13.0.x86_64/bin/bf-p4c -g "./lucid_tofino_build/lucid.p4"; } > bf-p4c_time.txt 2>&1
 
     # Extract the real time from the time command output
     bf_p4c_real_time=$(grep "real" bf-p4c_time.txt | awk '{print $2}')
 
     # Append bf-p4c runtime to results.txt
     echo "bf-p4c Runtime for $filename: $bf_p4c_real_time" >> results.txt
-    
+
     stages_count=$(grep -oE "Number of stages in table allocation: ([0-9]+)" "lucid.tofino/pipe/logs/table_summary.log" | sed 's/Number of stages in table allocation: //')
 
     # Append stages count to results.txt
@@ -41,8 +41,12 @@ while IFS= read -r filename; do
 
     echo "PHV Usage: $PHV_usage" >> results.txt
 
+    VLIW_usage=$(grep "Average" "lucid.tofino/pipe/logs/mau.resources.log" | cut -d "|" -f11)
+
+    echo "VLIW usage: $VLIW_usage" >> results.txt
+
     # Clean up temporary files
-    rm -f bf-p4c_time.txt output.txt
+    rm -f bf-p4c_time.txt
 
     # Separate results for each file
     echo "--------------------------" >> results.txt
