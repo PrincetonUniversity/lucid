ebb1ed8faf80e9c9d08bcaa589bcda8ab66ce8d4
Devon Loehr <dloehr@princeton.edu>
2023-10-04 13:52:50 -0400

    Update patch script for current syntax

--- examples/publications/sigcomm21/scripts/patch_script.py
+++ examples/publications/sigcomm21/scripts/patch_script.py
@@ -5,13 +5,30 @@ import sys
 
 rules = [
     (r'packet event (\w+)\s*\((.*?)\);', r'event \1(\2);'),
-    (r'exit event (\w+)\s*\((.*?)\);', r'event \1(\2){skip;}')
+    (r'exit event (\w+)\s*\((.*?)\);', r'event \1(\2) {skip;}'),
+    (r'group (\w+)\s*=', r'const group \1 ='),
+    (r'mgenerate Event\.(s|m)mlocate\((.+),\s*(\w+)\);', r'generate_ports (\3, \2);'),
+    (r'generate Event\.(s|m)slocate\((.+),\s*(\w+)\);', r'generate_port ((int<9>)\3, \2);'),
+    # Special cases
+    # dnsguard
+    (r'event dnspkt_out\s*\(int<1> packet_allowed',
+     r'event dnspkt_out (int<1> packet_allowed, int<31> padding'),
+    (r'dnspkt_out\(TRUE', r'dnspkt_out(TRUE, 0'),
+    # chain_stateful_firewall
+    (r'const int (head|tail|succ)', r'const int<9> \1'),
+    (r'int self', r'int<9> self'),
+    # Undo the above for rip
+    (r'const int<9> self', r'const int self'),
+    # countmin_historical + starflow
+    (r'memop read', r'memop fst'),
+    (r'Array.update\((.+), read, (.+)\);', r'Array.update(\1, fst, \2);'),
+
     # Add more rules as needed
 ]
 
 def patch_file(file_path):
     print(f"Patching file: {file_path}")
-    
+
     with open(file_path, 'r') as file:
         content = file.read()
 
