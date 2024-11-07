ff28683ae1f3ef53942ee73ff5d5275a9ea487b7
John Sonchack <john.sonchack@gmail.com>
2024-11-07 14:21:42 -0500

    test with reflector on simulator.

--- examples/utils/generator/cmd.py
+++ examples/utils/generator/cmd.py
@@ -55,7 +55,7 @@ def handle_report(raw_ev):
         loss = 1.0 - (float(rxct) / float(txct))
         print("loss = %s" % loss)
     
-def send_pkt(port, ct, src="00:11:22:33:44:55", dst="07:08:09:10:11:12", et="08:00", data="bb:aa:dd:aa:ff"):
+def send_pkt(port, ct, src="00:11:22:33:44:55", dst="01:01:01:01:01:01", et="08:00", data="bb:aa:dd:aa:ff"):
     print("generating command to send %s packets" % ct)
     ct_bytes = unbbytes(ct.to_bytes(2, byteorder='big'))
     port_bytes = unbbytes(port.to_bytes(2, byteorder='big'))
