67c103b9b3413fda310998f1d0b68c6dcf9e52ce
John Sonchack <john.sonchack@gmail.com>
2024-11-08 09:50:42 -0500

    multi flow tested build

--- examples/utils/generator/test.sh
+++ examples/utils/generator/test.sh
@@ -3,7 +3,10 @@
 # sleep 10
 # 2. run reflector
 sudo ./reflector -i veth297 &
-# 3. generate a packet using cmd.py
-sudo ./cmd.py send 148 6
+# 3. initialize a flow or two
+sudo ./cmd.py init 148 6
+sudo ./cmd.py init 148 6
+# 4. start sending
+sudo ./cmd.py send 148
 # 4. query the counters using cmd.py
 sudo ./cmd.py query
